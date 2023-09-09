555561871650ab353e06bd1aa07cc0eed5461ba4
Christina <christina05301@gmail.com>
2023-09-09 13:28:13 -0400

    build word lists

---
+++ words.js
@@ -0,0 +1,18 @@
+const adj = ['avid', 'dull', 'chilly', 'spooky', 'harried', 'unkempt', 'flamboyant', 'odd-looking',  'cagey', 'green', 'glum', 'good', 'just', 'last', 'precise', 'profane', 'queenly', 'jubilant', 'yellow', 'arresting', 'crochety', 'expectant', 'visionary', 'whimsical', 'real', 'zany', 'lost', 'beige', 'sepia', 'empty', 'second', 'toothless', 'cagey', 'cheap', 'chief', 'civil', 'clean', 'scarlet', 'glowing', 'tall', 'poor', 'empty', 'mellow', 'middle', 'golden', 'honest', 'larger', 'parched', 'massive', 'plastic'];
+
+for (let i=0; i < adj.length; i++) {
+    console.log(adj[i]);
+}
+console.log(adj.length);
+
+const singularNouns  = ['man', 'tree', 'building', 'box', 'bobcat', 'train', 'river', 'bridge', 'mountain', 'teacup', 'songbird', 'suitcase', 'carpet', 'map', 'bicycle', 'grass', 'hummingbird', 'square', 'highway', 'book', 'postcard', 'letter', 'desk', 'pumpkin', 'star',    ];
+
+const pluralNouns = [];
+
+const singularVerbs = [];
+
+const pluralVerbs = [];
+
+
+
+
