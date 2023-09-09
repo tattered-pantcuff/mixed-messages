a586f6e840fea05ec592c360a127c010c4ec394f
Christina <christina05301@gmail.com>
2023-09-09 15:46:26 -0400

    write messages function

--- words.js
+++ words.js
@@ -24,7 +24,7 @@ console.log(`The ${adj[2]} ${singularNouns[3]} ${pluralVerbs[2]} the ${singularN
 const words = {
     adj: ['avid', 'dull', 'chilly', 'spooky', 'harried', 'unkempt', 'flamboyant', 'odd-looking',  'cagey', 'green', 'glum', 'good', 'just', 'last', 'precise', 'profane', 'queenly', 'jubilant', 'yellow', 'arresting', 'crochety', 'expectant', 'visionary', 'whimsical', 'real', 'zany', 'lost', 'beige', 'sepia', 'empty', 'second', 'toothless', 'cagey', 'cheap', 'chief', 'civil', 'clean', 'scarlet', 'glowing', 'tall', 'poor', 'empty', 'mellow', 'middle', 'golden', 'honest', 'larger', 'parched', 'massive', 'plastic'],
     noun: ['man', 'tree', 'building', 'box', 'bobcat', 'train', 'river', 'bridge', 'mountain', 'teacup', 'songbird', 'suitcase', 'carpet', 'map', 'bicycle', 'grass', 'hummingbird', 'square', 'highway', 'book', 'postcard', 'letter', 'desk', 'pumpkin', 'star', 'castle', 'shore', 'sword', 'stagecoach', 'roadster', 'chapel', 'saint', 'shrine', 'storm', 'laundry', 'basket', 'garden', 'window', 'table', 'trail', 'key', 'ink', 'pencil', 'bell', 'wallet', 'picture', 'shell', 'stage', 'dream', 'bowl'],
-    verb: ['wants', 'looks at', 'uses', 'is', 'says', 'goes', 'gets', 'makes', 'knows', 'thinks', 'takes', 'sees', 'works', 'calls', 'tries', 'asks', 'needs', 'seems', 'helps', 'talks', 'turns', 'starts', 'shows', 'plays', 'moves', 'likes', 'lives', 'believes', 'happens', 'provides', 'includes', 'continues past', 'changes', 'watches', 'follows', 'stops', 'creates', 'allows', 'adds', 'opens', 'walks', 'offers', 'remembers', 'loves', 'considers', 'appears before', 'awaits', 'serves', 'expects', 'stays', 'reaches', 'remains near', 'suggests', 'raises', 'passes', 'requires', 'reports', 'decides for', 'pulls', 'paints'],
+    verb: ['wants', 'looks at', 'uses', 'is', 'says', 'goes', 'gets', 'makes', 'knows', 'thinks', 'takes', 'sees', 'works', 'calls', 'tries', 'asks', 'needs', 'seems like', 'helps', 'talks', 'turns', 'starts', 'shows', 'plays', 'moves', 'likes', 'lives', 'believes', 'happens', 'provides', 'includes', 'continues past', 'changes', 'watches', 'follows', 'stops', 'creates', 'allows', 'adds', 'opens', 'walks', 'offers', 'remembers', 'loves', 'considers', 'appears before', 'awaits', 'serves', 'expects', 'stays', 'reaches', 'remains near', 'suggests', 'raises', 'passes', 'requires', 'reports', 'decides for', 'pulls', 'paints'],
     conjunction: ['and', 'but', 'or', 'so', 'yet', 'because', 'if', 'when', 'since', 'while'],
     returnRandomAdj() {
         let num = Math.floor(Math.random() * this.adj.length);
@@ -46,8 +46,12 @@ const words = {
 
 
 
-console.log(words.returnRandomAdj());
-console.log(words.returnRandomNoun());
-console.log(words.returnRandomVerb());
-console.log(words.returnRandomConjunction());
+//console.log(words.returnRandomAdj());
+//console.log(words.returnRandomNoun());
+//console.log(words.returnRandomVerb());
+//console.log(words.returnRandomConjunction());
 
+const poeticMessages = () => {
+    console.log('The ' + words.returnRandomAdj() + ' ' + words.returnRandomNoun() + ' ' + words.returnRandomVerb() + ' ' + words.returnRandomConjunction() + ' ' + 'the ' + words.returnRandomAdj() + ' ' + words.returnRandomNoun() + ' ' + words.returnRandomVerb() + ' ' + 'the ' + words.returnRandomNoun() + '.')
+};
+poeticMessages();
