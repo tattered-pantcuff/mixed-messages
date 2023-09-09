d062e67168f0decf7ac7fd9e7cc5d25fdb135803
Christina <christina05301@gmail.com>
2023-09-09 13:58:09 -0400

    add words to arrays

--- words.js
+++ words.js
@@ -1,17 +1,25 @@
 const adj = ['avid', 'dull', 'chilly', 'spooky', 'harried', 'unkempt', 'flamboyant', 'odd-looking',  'cagey', 'green', 'glum', 'good', 'just', 'last', 'precise', 'profane', 'queenly', 'jubilant', 'yellow', 'arresting', 'crochety', 'expectant', 'visionary', 'whimsical', 'real', 'zany', 'lost', 'beige', 'sepia', 'empty', 'second', 'toothless', 'cagey', 'cheap', 'chief', 'civil', 'clean', 'scarlet', 'glowing', 'tall', 'poor', 'empty', 'mellow', 'middle', 'golden', 'honest', 'larger', 'parched', 'massive', 'plastic'];
 
-for (let i=0; i < adj.length; i++) {
-    console.log(adj[i]);
-}
-console.log(adj.length);
-
-const singularNouns  = ['man', 'tree', 'building', 'box', 'bobcat', 'train', 'river', 'bridge', 'mountain', 'teacup', 'songbird', 'suitcase', 'carpet', 'map', 'bicycle', 'grass', 'hummingbird', 'square', 'highway', 'book', 'postcard', 'letter', 'desk', 'pumpkin', 'star',    ];
+const singularNouns  = ['man', 'tree', 'building', 'box', 'bobcat', 'train', 'river', 'bridge', 'mountain', 'teacup', 'songbird', 'suitcase', 'carpet', 'map', 'bicycle', 'grass', 'hummingbird', 'square', 'highway', 'book', 'postcard', 'letter', 'desk', 'pumpkin', 'star', 'castle', 'shore', 'sword', 'stagecoach', 'roadster', 'chapel', 'saint', 'shrine', 'storm', 'laundry', 'basket', 'garden', 'window', 'table', 'trail', 'key', 'ink', 'pencil', 'bell', 'wallet', 'picture', 'shell', 'stage', 'dream', 'bowl'];
 
 const pluralNouns = [];
 
 const singularVerbs = [];
 
 const pluralVerbs = [];
+
+
+const listWords = arr => {
+    console.log(arr.length);
+    for (let i=0; i < arr.length; i++) {
+    console.log(arr[i]);
+    }
+};
+
+
+
+
+
 
 
 
